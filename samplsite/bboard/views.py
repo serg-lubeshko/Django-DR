ffa3b5c8463147943f6eaaa273ab68ea96fd1ab1
Sergei_L <posse7297@gmail.com>
2021-06-01 17:04:58 +0300

    create bboard

--- samplsite/bboard/views.py
+++ samplsite/bboard/views.py
@@ -2,7 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+
+from bboard.forms import BbForm
 from bboard.models import Bb, Rubric
+from django.views.generic.edit import CreateView
 
 
 # def index(request):
@@ -15,13 +19,26 @@ from bboard.models import Bb, Rubric
 
 def index(request):
     bbs = Bb.objects.all()
-    context = {'bbs': bbs}
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
     return render(request, template_name="bboard/index.html", context=context)
 
 
-def rubric_id(request, rubric_id):
+def by_rubric(request, rubric_id):
     bbs = Bb.objects.filter(rubric=rubric_id)
     rubrics = Rubric.objects.all()
     current_rubric = Rubric.objects.get(pk=rubric_id)
-    context = {'bbs':bbs,'rubrics':rubrics,'current_rubric':current_rubric}
-    return render(request, template_name='bboard/index.html', context=context)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, template_name='bboard/by_rubric.html', context=context)
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.htm.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics']  = Rubric.objects.all()
+        return context
+
+
